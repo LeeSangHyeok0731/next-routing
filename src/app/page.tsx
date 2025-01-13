bc157bf12dbd8a2e458450263bab9a5589417853
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-01-13 22:53:13 +0900

    카운트 증감 버튼 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,35 @@
 "use client"; // 클라이언트 컴포넌트 지시어 추가
 
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Home() {
   const router = useRouter();
 
+  const [count, setCount] = useState<number>(0);
+
   const handleClickPost = () => {
-    router.push("/post/1"); // '/next' 페이지로 이동
+    router.push(`/post/${count}`); // '/next' 페이지로 이동
   };
 
   const handleClickLogin = () => {
     router.push("/login");
   };
 
+  const countUp = () => {
+    setCount(count + 1);
+  };
+
+  const countDown = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
   return (
     <>
       <h1>라우팅 시험</h1>
+      <div onClick={countUp}>증가</div>
+      <div onClick={countDown}>감소</div>
       <button onClick={handleClickPost}>Post 페이지 가기</button>
       <button onClick={handleClickLogin}>Login 페이지 가기</button>
     </>
