185ca1b85a516802109d370641689f7b88c7cdac
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-01-13 22:40:19 +0900

    클릭시 홈으로 이동

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,17 @@
 "use client";
+import { useRouter } from "next/router";
 
 export default function Login() {
+  const router = useRouter();
+
+  const handleClickHome = () => {
+    router.push("/");
+  };
+
   return (
     <>
       <h1>로그인</h1>
+      <button onClick={handleClickHome}>홈으로 가기</button>
     </>
   );
 }
