395670a44cead907ba8bf09f9b467ea3879e805d
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-01-13 22:40:10 +0900

    클릭시 경로 이동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import { useRouter } from "next/router";
+
 export default function Home() {
+  const router = useRouter();
+
+  const handleClickPost = () => {
+    router.push("/post/[1]"); // '/next' 페이지로 이동
+  };
+
+  const handleClickLogin = () => {
+    router.push("/login");
+  };
+
   return (
     <>
       <h1>라우팅 시험</h1>
+      <button onClick={handleClickPost}>Post 페이지 가기</button>
+      <button onClick={handleClickLogin}>Login 페이지 가기</button>
     </>
   );
 }
